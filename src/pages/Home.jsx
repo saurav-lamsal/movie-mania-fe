5a87887d90d080323f38b3a0f89c2f1d3bf70ef1
saurav.lamsal <saurav.lamsal@codehimalaya.net>
2025-04-25 18:41:40 +0545

    cached removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../card/Card';
-import useFetchMovies from '../services/fetchdata';
+import React, { useEffect, useState } from "react";
+import Card from "../card/Card";
+import useFetchMovies from "../services/fetchdata";
 
 const Home = ({ searchTerm }) => {
   const [searchData, setSearchData] = useState([]);
   const [favorites, setFavorites] = useState([]);
-  
-  const url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-  const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
- 
-  const {data: movieData, isLoading}= useFetchMovies(url)
 
-  console.log(movieData,"data");
+  const url =
+    "discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+  const searchUrl = `search/movie?query=${searchTerm}&include_adult=false&language=en-US&page=1`;
+
+  const { data: movieData, isLoading } = useFetchMovies(url);
+
+  console.log(movieData, "data");
 
   const getSearchMovies = async () => {
     try {
       const res = await fetch(searchUrl, {
         method: "GET",
         headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTYzMGZlOTkzNzM1NGNmYjM3MjhjMjI5NDhmOWE0MyIsIm5iZiI6MTc0MzUxMTM3OC43MTgwMDAyLCJzdWIiOiI2N2ViZGY1MjU4ZTBhYTIzMzhmYjEwMjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3-eh4jUswnQR6RwsBMo6QvFKYQH98sGxeMzLem_OK64'
-        }
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTYzMGZlOTkzNzM1NGNmYjM3MjhjMjI5NDhmOWE0MyIsIm5iZiI6MTc0MzUxMTM3OC43MTgwMDAyLCJzdWIiOiI2N2ViZGY1MjU4ZTBhYTIzMzhmYjEwMjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3-eh4jUswnQR6RwsBMo6QvFKYQH98sGxeMzLem_OK64",
+        },
       });
       const data = await res.json();
       setSearchData(data.results);
@@ -31,9 +33,8 @@ const Home = ({ searchTerm }) => {
 
   useEffect(() => {
     const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
-    setFavorites( );
+    setFavorites();
   }, []);
-
 
   useEffect(() => {
     if (searchTerm) {
@@ -57,35 +58,32 @@ const Home = ({ searchTerm }) => {
   // Render a loader if loading
   if (isLoading) {
     return <p style={{ color: "white", textAlign: "center" }}>Loading...</p>;
-  } 
+  }
 
   return (
-
-    <div className='display-card'>
-      {!searchTerm ? (
-        movieData.map((movie) => (
-          <Card
-            key={movie.id}
-            title={movie.title}
-            id={movie.id}
-            content={movie.overview}
-            genre={movie.release_date}
-            addFavourite={() => handleFavorite(movie)}
-            imgSrc={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-          />
-        ))
-      ) : (
-        searchData.map((movie) => (
-          <Card
-            key={movie.id}
-            title={movie.title}
-            id={movie.id}
-            content={movie.overview}
-            genre={movie.release_date}
-            imgSrc={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-          />
-        ))
-      )}
+    <div className="display-card">
+      {!searchTerm
+        ? movieData.map((movie) => (
+            <Card
+              key={movie.id}
+              title={movie.title}
+              id={movie.id}
+              content={movie.overview}
+              genre={movie.release_date}
+              addFavourite={() => handleFavorite(movie)}
+              imgSrc={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            />
+          ))
+        : searchData.map((movie) => (
+            <Card
+              key={movie.id}
+              title={movie.title}
+              id={movie.id}
+              content={movie.overview}
+              genre={movie.release_date}
+              imgSrc={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+            />
+          ))}
     </div>
   );
 };
--- src/services/fetchdata.js
+++ src/services/fetchdata.js
@@ -1,29 +1,33 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
+const baseUrl = import.meta.env.VITE_BASE_URL;
 
 //creating custom hook for fetching the movie data
 const useFetchMovies = (url) => {
-    const [data, setData]= useState([])
-    const [isLoading, setIsLoading]= useState(true)
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const urlname = `${baseUrl}/${url}`;
 
-    useEffect(()=>{
-        const getData = async()=>{
-            setIsLoading(true)
-            const res = await fetch(url, {
-                method: "GET",
-                headers: {
-                  accept: 'application/json',
-                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTYzMGZlOTkzNzM1NGNmYjM3MjhjMjI5NDhmOWE0MyIsIm5iZiI6MTc0MzUxMTM3OC43MTgwMDAyLCJzdWIiOiI2N2ViZGY1MjU4ZTBhYTIzMzhmYjEwMjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3-eh4jUswnQR6RwsBMo6QvFKYQH98sGxeMzLem_OK64'
-                }
-              });
-              const json = await res.json()
-              setData(json.results)
-              setIsLoading(false);
-        }
-        getData()
-    },[url])
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch(urlname, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OTYzMGZlOTkzNzM1NGNmYjM3MjhjMjI5NDhmOWE0MyIsIm5iZiI6MTc0MzUxMTM3OC43MTgwMDAyLCJzdWIiOiI2N2ViZGY1MjU4ZTBhYTIzMzhmYjEwMjgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.3-eh4jUswnQR6RwsBMo6QvFKYQH98sGxeMzLem_OK64",
+        },
+      });
+      const json = await res.json();
+      setData(json.results);
+      setIsLoading(false);
+    };
+    getData();
+  }, [url]);
   return {
-    data, isLoading
-  }
-}
-
-export default useFetchMovies
+    data,
+    isLoading,
+  };
+};
+export default useFetchMovies;
